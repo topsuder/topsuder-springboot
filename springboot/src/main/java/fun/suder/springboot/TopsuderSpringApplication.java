e14e18ef9aa3b8394de50a856f22d058bbc14e23
topsuder <suders@vip.qq.com>
2022-07-14 11:36:11 +0800

    feat(TopsuderSpringboot): startTomcat

--- springboot/src/main/java/fun/suder/springboot/TopsuderSpringApplication.java
+++ springboot/src/main/java/fun/suder/springboot/TopsuderSpringApplication.java
@@ -1,5 +1,19 @@
 package fun.suder.springboot;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.Engine;
+import org.apache.catalina.Host;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Server;
+import org.apache.catalina.Service;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.core.StandardEngine;
+import org.apache.catalina.core.StandardHost;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
 /**
  * <span>Form File</span>
  * <p>Description</p>
@@ -15,7 +29,54 @@ package fun.suder.springboot;
 public class TopsuderSpringApplication {
 
     public static void run(Class clazz){
-        System.out.println("TopsuderSpringApplication.run()");
+        //创建一个spring容器
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+        applicationContext.register(clazz);
+        applicationContext.refresh();
+
+        //启动tomcat
+        startTomcat(applicationContext);
+
+    }
+
+    private static void startTomcat(AnnotationConfigWebApplicationContext applicationContext) {
+
+
+        Tomcat tomcat = new Tomcat();
+
+        Server server = tomcat.getServer();
+        Service service = server.findService("Tomcat");
+
+        Connector connector = new Connector();
+        connector.setPort(8081);
+
+        Engine engine = new StandardEngine();
+        engine.setDefaultHost("localhost");
+
+        Host host = new StandardHost();
+        host.setName("localhost");
+
+        String contextPath = "";
+        Context context = new StandardContext();
+        context.setPath(contextPath);
+        context.addLifecycleListener(new Tomcat.FixContextListener());
+
+        host.addChild(context);
+        engine.addChild(host);
+
+        service.setContainer(engine);
+        service.addConnector(connector);
+
+        tomcat.addServlet(contextPath, "dispatcher", new DispatcherServlet(applicationContext));
+        context.addServletMappingDecoded("/*", "dispatcher");
+
+        try {
+            tomcat.start();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
