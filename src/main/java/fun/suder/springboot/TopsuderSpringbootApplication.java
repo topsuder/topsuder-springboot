d2c648f9094a1ff54106ed44e1858452a3ea0876
topsuder <suders@vip.qq.com>
2022-07-14 10:48:19 +0800

    build(TopsuderSpringboot): 初始提交

---
+++ src/main/java/fun/suder/springboot/TopsuderSpringbootApplication.java
@@ -0,0 +1,13 @@
+package fun.suder.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopsuderSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopsuderSpringbootApplication.class, args);
+    }
+
+}
